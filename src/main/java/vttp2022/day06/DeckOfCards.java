28c40de00fe8ada211f4e8c56605d2c5786cd96a
Irfan <irfanimanabub@gmail.com>
2022-06-26 21:06:56 +0800

    initial commit

---
+++ src/main/java/vttp2022/day06/DeckOfCards.java
@@ -0,0 +1,12 @@
+package vttp2022.day06;
+
+public class DeckOfCards {
+    
+    /*public static final int NUMCARDS = 52;
+    private Card[] deckOfCards;
+
+    public DeckOfCards() {
+        deckOfCards = new Card[NUMCARDS];
+    } */
+
+}
